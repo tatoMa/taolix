d1774467ad01276c1b7f23be59b71e14f6b60421
Tato <jason.ma1904@gmail.com>
2021-12-31 22:05:46 +1100

    fixed player pointer event issue

--- components/Player.js
+++ components/Player.js
@@ -5,7 +5,7 @@ const ReactPlayer = dynamic(() => import("react-player/lazy"));
 
 const Player = ({ url, setPlay }) => {
   return (
-    <div className="flex h-full w-full absolute top-0 left-0 items-center pb-14 bg-black/50 z-10 ">
+    <div className="flex h-full w-full absolute top-0 left-0 items-center pb-14 bg-black/50 z-10 pointer-events-auto ">
       <XIcon
         className="h-10 w-10 text-blue-white absolute right-4 top-12 bg-black cursor-pointer pointer-events-auto hover:bg-white hover:text-black animate-fadeIn z-50"
         onClick={() => {
