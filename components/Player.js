e6cbb3e9a0da696b9cc7f7b52f48d7f60ca538dd
Tato <jason.ma1904@gmail.com>
2022-01-26 15:34:37 +1100

    add keyboard control for player (fast/back forward, mute and pause

--- components/Player.js
+++ components/Player.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
 
 export const VideoJS = (props) => {
-  const videoRef = React.useRef(null);
-  const playerRef = React.useRef(null);
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
   const { options, onReady } = props;
 
-  React.useEffect(() => {
+  useEffect(() => {
     require("videojs-contrib-hls.js");
 
     // make sure Video.js player is only initialized once
@@ -15,21 +15,48 @@ export const VideoJS = (props) => {
       const videoElement = videoRef.current;
       if (!videoElement) return;
 
-      const player = (playerRef.current = videojs(videoElement, options, () => {
-        console.log("player is ready");
-        onReady && onReady(player);
-      }));
-    } else {
-      // you can update player here [update player through props]
-      // const player = playerRef.current;
-      // player.autoplay(options.autoplay);
-      // player.src(options.sources);
+      const player = (playerRef.current = videojs(
+        videoElement,
+        {
+          ...options,
+          userActions: {
+            hotkeys: function (event) {
+              // `space` key = pause
+              if (event.which === 32) {
+                // this.playing ? this.play() : this.pause();
+                this.paused() ? this.play() : this.pause();
+              }
+              // left arror back forward
+              if (event.which === 37) {
+                this.currentTime(this.currentTime() - 10);
+              }
+              // right arror fast forward
+              if (event.which === 39) {
+                this.currentTime(this.currentTime() + 10);
+              }
+              // m key for mute and unmute
+              if (event.which === 77) {
+                this.muted() ? this.muted(false) : this.muted(true);
+              }
+            },
+          },
+        },
+        () => {
+          console.log("player is ready");
+          onReady && onReady(player);
+        }
+      ));
     }
   }, [options, videoRef]);
 
   // Dispose the Video.js player when the functional component unmounts
-  React.useEffect(() => {
+  useEffect(() => {
     const player = playerRef.current;
+
+    // auto focus on video player
+    if (videoRef.current) {
+      videoRef.current.focus();
+    }
 
     return () => {
       if (player) {
@@ -41,7 +68,11 @@ export const VideoJS = (props) => {
 
   return (
     <div data-vjs-player>
-      <video ref={videoRef} className="video-js vjs-big-play-centered" />
+      <video
+        ref={videoRef}
+        style={{ outline: "none" }}
+        className="video-js vjs-big-play-centered !outline-0 focus:outline-0 active:outline-0 hover:outline-0"
+      />
     </div>
   );
 };
