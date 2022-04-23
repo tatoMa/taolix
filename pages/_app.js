e8bc4c2d0017a1cdb622f000022b07b203caa17a
Tato <jason.ma1904@gmail.com>
2022-04-23 12:13:03 +1000

    removed framer-motion library

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import Layout from "../components/Layout/index";
 import GoogleAnalytics from "../components/Layout/GoogleAnalytic";
-import { motion } from "framer-motion";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps }, router }) {
@@ -16,25 +16,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps }, router }) {
       <GoogleAnalytics Component pageProps />
       {/* layout of app */}
       <Layout>
-        {/* global animated transition by changing url. Only for main section. Not including navbar and footer */}
-        <motion.div
-          key={router.asPath}
-          initial="initial"
-          animate="animate"
-          variants={{
-            initial: {
-              opacity: 0,
-              x: -200,
-            },
-            animate: {
-              opacity: 1,
-              x: 0,
-            },
-          }}
-        >
-          {/* main section of all pages */}
-          <Component {...pageProps} />
-        </motion.div>
+        {/* main section of all pages */}
+        <Component {...pageProps} />
       </Layout>
     </SessionProvider>
   );
