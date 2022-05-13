6591a83ed440d53ad1b2cc27d10f5c6076f5d2aa
Tato <jason.ma1904@gmail.com>
2022-05-13 20:55:16 +1000

    update index page with new Douban lists

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ import GroupSwiper from "../components/GroupSwiper";
 
 export default function Home({
   selectedVideosForHero,
-  videosHotListDoubanFiltered,
-  videosNewAll,
-  videosNewAction,
+  doubanHotTvList,
+  doubanHotMovieList,
+  doubanNewMovieList,
   videosNewCnTvShow,
   videosNewKrTvShow,
   videosNewUsTvShow,
@@ -27,22 +27,22 @@ export default function Home({
         <HeroSwiper top5={selectedVideosForHero} />
         <div className="-translate-y-9">
           {/* Line Break  */}
-          <LineBreak title="WHATS HOT" />
+          <LineBreak title="WHATS ON MOVIES" />
 
           {/* Video List Section */}
-          <GroupSwiper videos={videosHotListDoubanFiltered} />
+          <GroupSwiper videos={doubanHotMovieList} />
 
           {/* Line Break  */}
-          <LineBreak title="NEW ARRIVALS" />
+          <LineBreak title="WHATS ON TV" />
 
           {/* Video List Section */}
-          <GroupSwiper videos={videosNewAll} />
+          <GroupSwiper videos={doubanHotTvList} />
 
           {/* Line Break  */}
-          <LineBreak title="ACTION MOVIES" />
+          <LineBreak title="NEW MOVIES" />
 
           {/* Video List Section */}
-          <GroupSwiper videos={videosNewAction} />
+          <GroupSwiper videos={doubanNewMovieList} />
 
           {/* Line Break  */}
           <LineBreak title="CHINESE TV SHOWS" />
@@ -80,105 +80,161 @@ export default function Home({
 }
 
 export async function getStaticProps() {
+  let resultsPromiseAll;
+  try {
+    resultsPromiseAll = await Promise.allSettled([
+      // fetch(`${process.env.SITE_URL}/api/list`).then((res) => res.json()),
+      // fetch(`${process.env.SITE_URL}/api/list/2`).then((res) => res.json()),
+      fetch(`${process.env.SITE_URL}/api/list/15`).then((res) => res.json()),
+      fetch(`${process.env.SITE_URL}/api/list/19`).then((res) => res.json()),
+      fetch(`${process.env.SITE_URL}/api/list/18`).then((res) => res.json()),
+      fetch(`${process.env.SITE_URL}/api/list/22`).then((res) => res.json()),
+      fetch(`${process.env.SITE_URL}/api/list/28`).then((res) => res.json()),
+    ]);
+  } catch (error) {
+    console.error(error);
+  }
+
+  const successes = resultsPromiseAll
+    .filter((x) => x.status === "fulfilled")
+    .map((x) => x.value);
+
+  const failures = resultsPromiseAll
+    .filter((x) => x.status === "rejected")
+    .map((x) => x.reason);
+
+  // map and filter results for return needed
+  // const filteredByName = successes.map((item) => {
+  //   let temp = [];
+  //   temp[0] = item.list.find(
+  //     (item) =>
+  //       removeAllSpecialCharactersFromString(item.vod_name) === videoName
+  //   );
+  //   return { ...item, list: temp[0] !== undefined ? temp : [] };
+  // });
+
+  // asign all return needed data
+  const [
+    videosNewCnTvShow,
+    videosNewKrTvShow,
+    videosNewUsTvShow,
+    videosNewCnReality,
+    videosNewJpAnime,
+  ] = successes;
+
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
-  let videosNewAll = {};
-  try {
-    let response = await fetch(`${process.env.SITE_URL}/api/list`);
-    videosNewAll = await response.json();
-  } catch (e) {
-    console.error("error: ", e);
-  }
+  // let videosNewAll = {};
+  // try {
+  //   let response = await fetch(`${process.env.SITE_URL}/api/list`);
+  //   videosNewAll = await response.json();
+  // } catch (e) {
+  //   console.error("error: ", e);
+  // }
 
-  let videosNewAction = {};
-  try {
-    let response = await fetch(`${process.env.SITE_URL}/api/list/2`);
-    videosNewAction = await response.json();
-  } catch (e) {
-    console.error("error: ", e);
-  }
+  // let videosNewAction = {};
+  // try {
+  //   let response = await fetch(`${process.env.SITE_URL}/api/list/2`);
+  //   videosNewAction = await response.json();
+  // } catch (e) {
+  //   console.error("error: ", e);
+  // }
 
-  let videosNewCnTvShow = {};
-  try {
-    let response = await fetch(`${process.env.SITE_URL}/api/list/15`);
-    videosNewCnTvShow = await response.json();
-  } catch (e) {
-    console.error("error: ", e);
-  }
+  // let videosNewCnTvShow = {};
+  // try {
+  //   let response = await fetch(`${process.env.SITE_URL}/api/list/15`);
+  //   videosNewCnTvShow = await response.json();
+  // } catch (e) {
+  //   console.error("error: ", e);
+  // }
 
-  let videosNewKrTvShow = {};
-  try {
-    let response = await fetch(`${process.env.SITE_URL}/api/list/19`);
-    videosNewKrTvShow = await response.json();
-  } catch (e) {
-    console.error("error: ", e);
-  }
+  // let videosNewKrTvShow = {};
+  // try {
+  //   let response = await fetch(`${process.env.SITE_URL}/api/list/19`);
+  //   videosNewKrTvShow = await response.json();
+  // } catch (e) {
+  //   console.error("error: ", e);
+  // }
 
-  let videosNewUsTvShow = {};
-  try {
-    let response = await fetch(`${process.env.SITE_URL}/api/list/18`);
-    videosNewUsTvShow = await response.json();
-  } catch (e) {
-    console.error("error: ", e);
-  }
+  // let videosNewUsTvShow = {};
+  // try {
+  //   let response = await fetch(`${process.env.SITE_URL}/api/list/18`);
+  //   videosNewUsTvShow = await response.json();
+  // } catch (e) {
+  //   console.error("error: ", e);
+  // }
 
-  let videosNewCnReality = {};
-  try {
-    let response = await fetch(`${process.env.SITE_URL}/api/list/22`);
-    videosNewCnReality = await response.json();
-  } catch (e) {
-    console.error("error: ", e);
-  }
+  // let videosNewCnReality = {};
+  // try {
+  //   let response = await fetch(`${process.env.SITE_URL}/api/list/22`);
+  //   videosNewCnReality = await response.json();
+  // } catch (e) {
+  //   console.error("error: ", e);
+  // }
 
-  let videosNewJpAnime = {};
-  try {
-    let response = await fetch(`${process.env.SITE_URL}/api/list/28`);
-    videosNewJpAnime = await response.json();
-  } catch (e) {
-    console.error("error: ", e);
-  }
+  // let videosNewJpAnime = {};
+  // try {
+  //   let response = await fetch(`${process.env.SITE_URL}/api/list/28`);
+  //   videosNewJpAnime = await response.json();
+  // } catch (e) {
+  //   console.error("error: ", e);
+  // }
 
   // get hot movie list from douban API
   let videosHotListDouban = {};
-  try {
-    videosHotListDouban = await getVideosListFromDouban(
-      `${process.env.DOUBAN_URL}${encodeURI(
-        "/j/search_subjects?type=tv&tag=热门&sort=recommend&page_limit=50&page_start=0"
-      )}`
-    );
-  } catch (e) {
-    console.error("error: ", e);
-  }
 
-  // using Douban ranking video list fetch all individual resource from API
-  let videosHotListDoubanFindResource = await Promise.allSettled(
-    videosHotListDouban.map(async (item) => {
-      try {
-        const res = await findResourceFromDoubanItem(item);
-        return res;
-      } catch (e) {
-        console.error("error: ", e);
-      }
-    })
+  const gerVideoListFromDoubanApiHotList = async (url) => {
+    try {
+      videosHotListDouban = await getVideosListFromDouban(
+        `${process.env.DOUBAN_URL}${encodeURI(url)}`
+      );
+    } catch (e) {
+      console.error("error: ", e);
+    }
+
+    // using Douban ranking video list fetch all individual resource from API
+    let videosHotListDoubanFindResource = await Promise.allSettled(
+      videosHotListDouban.map(async (item) => {
+        try {
+          const res = await findResourceFromDoubanItem(item);
+          return res;
+        } catch (e) {
+          console.error("error: ", e);
+        }
+      })
+    );
+
+    // filter unnecessary items
+    const videosHotListDoubanFiltered = {};
+    videosHotListDoubanFiltered.list = videosHotListDoubanFindResource
+      .filter(Boolean)
+      .filter((item) => item.status === "fulfilled" && item.value !== undefined)
+      .map((item) => item.value);
+    return videosHotListDoubanFiltered;
+  };
+
+  const doubanHotTvList = await gerVideoListFromDoubanApiHotList(
+    "/j/search_subjects?type=tv&tag=热门&sort=recommend&page_limit=50&page_start=0"
+  );
+  const doubanHotMovieList = await gerVideoListFromDoubanApiHotList(
+    "/j/search_subjects?type=movie&tag=热门&sort=recommend&page_limit=50&page_start=0"
   );
 
-  // filter unnecessary items
-  const videosHotListDoubanFiltered = {};
-  videosHotListDoubanFiltered.list = videosHotListDoubanFindResource
-    .filter(Boolean)
-    .filter((item) => item.status === "fulfilled" && item.value !== undefined)
-    .map((item) => item.value);
+  const doubanNewMovieList = await gerVideoListFromDoubanApiHotList(
+    "/j/search_subjects?type=movie&tag=最新&sort=recommend&page_limit=50&page_start=0"
+  );
 
   const selectedVideosForHero = shuffle([
-    ...videosHotListDoubanFiltered.list,
+    ...doubanHotTvList.list,
+    ...doubanHotMovieList.list,
   ]).slice(0, 6);
 
   return {
     props: {
       selectedVideosForHero,
-      videosHotListDoubanFiltered,
-      videosNewAll,
-      videosNewAction,
+      doubanHotTvList,
+      doubanHotMovieList,
+      doubanNewMovieList,
       videosNewCnTvShow,
       videosNewKrTvShow,
       videosNewUsTvShow,
