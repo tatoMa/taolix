42d40b3107a9a64a4ee1fa37ec929e9568ea05db
Tato <jason.ma1904@gmail.com>
2022-07-07 16:13:12 +1000

    update: new UI installed and inited

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,14 +1,22 @@
 import { useEffect } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
+import { themeChange } from "theme-change";
 
 const test = () => {
   const [name, setName] = useLocalStorage("name", "");
   useEffect(() => {
+    themeChange(false);
     setName("Bob");
   }, []);
 
   return (
     <div>
+      <button className="btn btn-primary">Button</button>
+      <select data-choose-theme>
+        <option value="">Default</option>
+        <option value="dark">Dark</option>
+        <option value="cupcake">cupcake</option>
+      </select>
       <input
         className="mt-32"
         type="text"
