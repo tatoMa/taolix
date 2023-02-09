7d49aaa3cdcd96a2ddc328585164cb234808dae7
tato <jason.ma1904@gmail.com>
2023-02-09 11:32:48 +1100

    fix: source problem

--- components/VideoPlayList.tsx
+++ components/VideoPlayList.tsx
@@ -11,18 +11,21 @@ const VideoPlayList = ({
   index,
   playedUrls,
 }) => {
+  const isHdSource = index === "HD" || index === 3;
   return (
     <>
       {videoList && (
         <div className={`z-10`}>
           <div
             className={`my-2 mt-4 inline-block rounded-full  px-6 py-1 text-sm  ${
-              index === "HD"
+              isHdSource
                 ? " text-neutral-700 bg-yellow-400"
                 : "bg-red-500 text-white"
             }`}
           >
-            {index === "HD" ? "1080P " : `Source ${index}`}
+            {isHdSource
+              ? `1080P ${index === "HD" ? 1 : 2} `
+              : `Source ${index}`}
             {index === "HD" && <span className="text-red-600 ">🚫 PRC IP</span>}
           </div>
           <div
@@ -36,36 +39,42 @@ const VideoPlayList = ({
                 <a
                   key={url}
                   className={`btn btn-outline btn-square btn-block justify-start rounded-none ${
-                    index === "HD"
+                    isHdSource
                       ? " border-y border-x-0 border-yellow-600 text-yellow-600"
                       : "border-y border-x-0 border-base-content"
                   }`}
                   onClick={() => {
-                    if(index === "HD"){
-                      window.open("https://dp.haiwaikan.com/index.html?url="+url, '_blank', 'noopener,noreferrer');
-                    }
-                    else{
-                      clickUrlButton(url);
-                      document.documentElement.style.overflowY = "hidden";
-                    }
+                    clickUrlButton(url);
+                    document.documentElement.style.overflowY = "hidden";
+                    // THIS IS FOR WHEN HD VIDEO NOT PLAY ON DEFAULT PLAYER
+                    // if (isHdSource) {
+                    //   window.open(
+                    //     "https://dp.haiwaikan.com/index.html?url=" + url,
+                    //     "_blank",
+                    //     "noopener,noreferrer"
+                    //   );
+                    // } else {
+                    //   clickUrlButton(url);
+                    //   document.documentElement.style.overflowY = "hidden";
+                    // }
                   }}
                 >
                   <div>
                     {playedUrls.includes(url) ? (
                       <CheckCircleIcon
                         className={`ml-1 mr-4 h-8 w-8 text-gray-500 duration-100 group-hover:translate-x-2 group-hover:text-white md:ml-2 ${
-                          index === "HD" && "text-yellow-600 "
+                          isHdSource && "text-yellow-600 "
                         }`}
                       />
                     ) : (
                       <PlayIcon
                         className={`ml-1 mr-4 h-8 w-8 text-gray-500 duration-100 group-hover:translate-x-2 group-hover:text-white md:ml-2 ${
-                          index === "HD" && "text-yellow-600 "
+                          isHdSource && "text-yellow-600 "
                         }`}
                       />
                     )}
                   </div>
-                  <div className="font-light truncate">{name}</div>
+                  <div className="truncate font-light">{name}</div>
                 </a>
               );
             })}
