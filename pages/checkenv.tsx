7c11d13dea23872e58eff0b5e17389148cbc3465
tato <jason.ma1904@gmail.com>
2023-05-05 12:00:17 +1000

    feat: env checker

---
+++ pages/checkenv.tsx
@@ -0,0 +1,18 @@
+import { GetServerSideProps } from "next";
+import { APILIST } from "utils/const";
+
+const checkenv = ({ allApiLists }) => {
+  return <div className="mt-32">{JSON.stringify(allApiLists)}</div>;
+};
+
+export default checkenv;
+
+export const getStaticProps: GetServerSideProps = async (context) => {
+  let allApiLists = APILIST;
+  console.log(allApiLists);
+  return {
+    props: {
+      allApiLists,
+    },
+  };
+};
