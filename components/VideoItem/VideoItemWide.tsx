3c866803d258c4591bc1470bdf68a3d901b87c40
tato <jason.ma1904@gmail.com>
2023-08-24 15:59:39 +1000

    fix: wide-item-loading-remove

--- components/VideoItem/VideoItemWide.tsx
+++ components/VideoItem/VideoItemWide.tsx
@@ -22,29 +22,6 @@ const VideoItem = ({ name, imageUrl, id, resource }) => {
           }`}
         >
           <a className="relative block overflow-hidden transition duration-500 cursor-pointer group hover:saturate-150">
-            <div className="absolute top-0 left-0 z-10 flex items-center justify-center w-full h-full bg-gray-500/60">
-              <svg
-                viewBox="-2 -2 42 42"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-1/2 mb-5 aspect-square stroke-red-500"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <g transform="translate(1 1)" strokeWidth="5">
-                    <circle strokeOpacity=".4" cx="18" cy="18" r="18" />
-                    <path d="M36 18c0-9.94-8.06-18-18-18">
-                      <animateTransform
-                        attributeName="transform"
-                        type="rotate"
-                        from="0 18 18"
-                        to="360 18 18"
-                        dur="1s"
-                        repeatCount="indefinite"
-                      />
-                    </path>
-                  </g>
-                </g>
-              </svg>
-            </div>
             <div className="relative aspect-[16/10] w-full">
               <img
                 src={imageUrl}
