5e9489fa214df7a055580f01844b5b78e2531696
tato <jason.ma1904@gmail.com>
2023-08-25 12:24:28 +1000

    refactor: router-loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import GoogleAnalytics from "../components/Layout/GoogleAnalytic";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Session } from "next-auth";
-import { useRouter } from "next/router";
+import useRouterLoading from "hooks/useRouterLoading";
 import LoadingAnimation from "components/Layout/Loading";
 
 function MyApp({
@@ -15,36 +15,12 @@ function MyApp({
 }: AppProps<{
   session: Session;
 }>) {
-  const router = useRouter();
   const [loading, setLoading] = useState(false);
-  let timer = useRef(null);
+  useRouterLoading(setLoading);
+
   useEffect(() => {
     document.documentElement.lang = "en-us";
   });
-  useEffect(() => {
-    const handleStart = (url: string) => {
-      console.log(`Loading: ${url}`);
-      timer.current = setTimeout(() => {
-        setLoading(true);
-      }, 150);
-    };
-
-    const handleStop = (url: string) => {
-      console.log(`Done: ${url}`);
-      clearTimeout(timer.current);
-      setLoading(false);
-    };
-
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleStop);
-    router.events.on("routeChangeError", handleStop);
-
-    return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleStop);
-      router.events.off("routeChangeError", handleStop);
-    };
-  }, [router]);
 
   return (
     // Auth session component
